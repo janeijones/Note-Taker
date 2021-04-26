c6e884772c341cd526858c1ff45c13fc107e6d33
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-25 21:45:02 -0500

    made public folder static

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependecies
 const express = require('express');
+const path = require('path');
 
 //sets up express app
 const app = express();
@@ -8,9 +9,10 @@ const PORT = 3000;
 //sets up express app to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!');
+    // res.sendFile(path.join(__dirname,
 });
 
 
